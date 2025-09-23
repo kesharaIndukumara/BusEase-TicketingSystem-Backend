2d325a0057e97b4af588258d3f59297d4164b13d
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-09-24 00:44:49 +0530

    refactor: set folder structure of busowner server

---
+++ src/main/java/edu/icet/ecom/model/entity/BusOwnerEntity.java
@@ -0,0 +1,8 @@
+package edu.icet.ecom.model.entity;
+
+public class BusOwnerEntity {
+    private Long nic;
+    private String name;
+    private String vehicleRegNumber;
+    private String address;
+}
