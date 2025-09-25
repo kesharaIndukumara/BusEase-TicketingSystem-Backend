ccd52f6a70d72e40fc2ebd341a6fe1fdc248ac34
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-09-25 09:48:50 +0530

    Add customer service implementation and database configuration

--- src/main/java/edu/icet/ecom/service/impl/CustomerServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/impl/CustomerServiceImpl.java
@@ -1,11 +1,26 @@
 package edu.icet.ecom.service.impl;
 
 import edu.icet.ecom.model.dto.CustomerDTO;
+import edu.icet.ecom.model.entity.CustomerEntity;
+import edu.icet.ecom.repository.CustomerRepository;
 import edu.icet.ecom.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+
+@Service
+@RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
+
+    final ModelMapper modelMapper;
+
+    final CustomerRepository customerRepository;
+
     @Override
-    public CustomerDTO createCustomer(CustomerDTO customer) {
-        return null;
+    public CustomerDTO createCustomer(CustomerDTO customerDTO) {
+        CustomerEntity customerEntity = modelMapper.map(customerDTO, CustomerEntity.class);
+        CustomerEntity savedCustomer = customerRepository.save(customerEntity);
+        return modelMapper.map(savedCustomer, CustomerDTO.class);
     }
 }
