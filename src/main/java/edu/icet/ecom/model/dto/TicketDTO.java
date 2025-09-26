6199d6f794e5130482c892b04e1e9635a41faba1
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-09-26 21:14:02 +0530

    Add application configuration, ticket DTO and entity, and update project settings

--- src/main/java/edu/icet/ecom/model/dto/TicketDTO.java
+++ src/main/java/edu/icet/ecom/model/dto/TicketDTO.java
@@ -1,4 +1,13 @@
 package edu.icet.ecom.model.dto;
 
+import lombok.Data;
+
+@Data
 public class TicketDTO {
+    private Long id;
+
+    private String fullName;
+    private Integer contactNumber;
+    private String email;
+    private Integer sheetsCount;
 }
--- src/main/java/edu/icet/ecom/model/entity/TicketEntity.java
+++ src/main/java/edu/icet/ecom/model/entity/TicketEntity.java
@@ -1,4 +1,18 @@
 package edu.icet.ecom.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "customer_ticket")
 public class TicketEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private Integer contactNumber;
+    private String email;
+    private Integer sheetsCount;
 }
